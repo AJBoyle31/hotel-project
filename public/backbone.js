ad8c8ed3e7cd44d5a4bf85c2fd722f54d0b50980
Andrew Boyle <ajboyle31@gmail.com>
2016-07-01 00:33:56 +0000

    moved js file

---
+++ public/backbone.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#search').click(function(){
+        $('#searchbox').show();
+    });
+    
+
+    $('#submit').click(function(){
+        var loc = $('#selloc').val();
+        var checkin = $('#checkin').val();
+        var checkout = $('#checkout').val();
+        
+    });
+    
+    
+    
+    
+    
+})
