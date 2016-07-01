932a81e2cc11a2d8f36d238582ff987d45720562
Andrew Boyle <ajboyle31@gmail.com>
2016-07-01 00:39:36 +0000

    changed name

---
+++ public/application.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#search').click(function(){
+        $('#searchbox').show();
+    });
+    
+
+    $('#submit').click(function(){
+        var loc = $('#selloc').val();
+        var checkin = $('#checkin').val();
+        var checkout = $('#checkout').val();
+        
+    });
+    
+    
+})
+
