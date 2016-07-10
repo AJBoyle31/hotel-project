647c62bb62e51d22583f0f180690c210a5668609
Andrew Boyle <ajboyle31@gmail.com>
2016-07-10 02:13:14 +0000

    added slideshow code

--- public/application.js
+++ public/application.js
@@ -238,7 +238,11 @@ function hotelInfo(){
     $('#hotels').html(assemblyHotelsView.render().el);
     
     //fetch the data from the server
-    assemblyHotels.fetch();
+    assemblyHotels.fetch({
+        success: function(){
+            showDivs(slideIndex);
+        }
+    });
     
     
     
@@ -335,6 +339,23 @@ function storeId(hotelId){
     
 }
 
+var slideIndex = 1;
+
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideShow");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
 
 
 
