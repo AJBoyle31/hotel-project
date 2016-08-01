a6724ff21105c477e55167d333ea6f6c3c20d284
Andrew Boyle <ajboyle31@gmail.com>
2016-08-01 23:09:18 +0000

    changed from refs to target

--- public/source/GetRates.js
+++ public/source/GetRates.js
@@ -3,14 +3,13 @@ var React = require('react');
 //Get information from user regarding city, checkin and checkout dates. 
 var GetRates = React.createClass({
     getInfo: function(event){
-        event.preventDefault();
         var info = {
-            city: this.refs.city.value,
-            checkin: this.refs.checkin.value,
-            checkout: this.refs.checkout.value
+            city: event.target.city.value,
+            checkin: event.target.checkin.value,
+            checkout: event.target.checkout.value
         };
         this.props.updateInfo(info);
-        
+        event.preventDefault();
     },
     render: function(){
         return (
@@ -18,19 +17,19 @@ var GetRates = React.createClass({
             <h3 className="searchHotels">Search Hotels</h3>
             <form ref="formRates" className="formRates" onSubmit={this.getInfo}>
                 <label>City
-                <select ref="city" id="city">
+                <select name="city" id="city">
                     <option value='charlottesville'>Charlottesville</option>
                     <option value='newyork'>New York City</option>
                     <option value='chicago'>Chicago</option>
                 </select>
                 </label>
                 <label>Check In
-                <input type='text' ref='checkin' id='checkin' placeholder='YYYY-MM-DD' required />
+                <input type='text' name='checkin' id='checkin' placeholder='YYYY-MM-DD' required />
                 </label>
                 <label>Check Out
-                <input type='text' ref='checkout' id='checkout' placeholder='YYYY-MM-DD' required />
+                <input type='text' name='checkout' id='checkout' placeholder='YYYY-MM-DD' required />
                 </label>
-                <input type="submit" value="Get Rates" />
+                <button type="submit">Get Rates</button>
             </form>
             
             </div>
