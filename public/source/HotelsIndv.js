066e1ecffe7ffd30656b19e8f5700e45ffaf6dc0
Andrew Boyle <ajboyle31@gmail.com>
2016-08-05 17:20:27 +0000

    no changes made

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 
 
 class HotelsIndv extends Component {
+    
     render(){
-        
-        
         return (
             <div className="hotels">
-            <a href={this.props.link}>
+            <a href={this.props.link} >
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
