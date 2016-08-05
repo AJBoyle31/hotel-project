6eb323d647384581c2241cfdbeb8e46a64f82b4d
Andrew Boyle <ajboyle31@gmail.com>
2016-08-05 17:21:20 +0000

    added click functionality to nav

--- public/source/Nav.js
+++ public/source/Nav.js
@@ -1,8 +1,48 @@
 import React, {Component} from 'react';
 import GetRates from './GetRates';
+import Contact from './Contact';
+import Cities from './Cities';
 
 class Nav extends Component {
+    constructor(){
+        super();
+        this.state = {
+            showCities: false,
+            showSearch: true,
+            showContact: false
+        };
+    }
+    toggleCities(){
+        this.setState({
+            showCities: true,
+            showSearch: false,
+            showContact: false
+        });
+    }
+    toggleSearch(){
+        this.setState({
+            showCities: false,
+            showSearch: true,
+            showContact: false
+        });
+    }
+    toggleContact(){
+        this.setState({
+            showCities: false,
+            showSearch: false,
+            showContact: true
+        });
+    }
     render(){
+        var homepage = '';
+        if (this.state.showCities) {
+            homepage = <Cities />;
+        }
+        else if (this.state.showContact){
+            homepage = <Contact />;
+        }
+        else { homepage = <GetRates /> }
+        
         return (
             <div className='navsearch'>
             <nav className='nav navbar'>
@@ -19,15 +59,15 @@ class Nav extends Component {
                 <div className='collapse navbar-collapse'>
                     <ul className='nav navbar-nav navbar'>
                         <li className='navlinks'><a href='/'>Home</a></li>
-                        <li className='navlinks'><a href='#cities' id='cities'>Cities We Represent</a></li>
-                        <li className='navlinks'><a href='#search' id='search'>Search Hotels</a></li>
-                        <li className='navlinks'><a href='#contact' id='contact'>Contact Us</a></li>
+                        <li className='navlinks'><a href='#cities' id='cities' onClick={this.toggleCities.bind(this)} >Cities We Represent</a></li>
+                        <li className='navlinks'><a href='#search' id='search' onClick={this.toggleSearch.bind(this)} >Search Hotels</a></li>
+                        <li className='navlinks'><a href='#contact' id='contact' onClick={this.toggleContact.bind(this)} >Contact Us</a></li>
                     </ul>
                 </div>
             </div>        
         </nav> 
         
-        <GetRates />
+        {homepage}
         
         </div>
         );
