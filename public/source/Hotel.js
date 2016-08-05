46a7f36c6f739e16dfd45fff620fda83eda276f4
Andrew Boyle <ajboyle31@gmail.com>
2016-08-05 01:03:01 +0000

    for a single hotel api call

---
+++ public/source/Hotel.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Hotel extends Component {
+    render(){
+        return (
+            <div>test</div>
+        );
+    }
+}
+
+export default Hotel;
