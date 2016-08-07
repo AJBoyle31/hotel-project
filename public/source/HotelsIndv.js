40740efd7675c9f321be82e2f2982a911f33ea5d
Andrew Boyle <ajboyle31@gmail.com>
2016-08-07 17:15:33 +0000

    working on showing indv hotel

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 
 
 class HotelsIndv extends Component {
-    
+    showHotel(){
+        
+    }
     render(){
         return (
             <div className="hotels">
-            <a href={this.props.link} >
+            <a href={this.props.link} onClick={this.showHotel.bind(this)}>
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
