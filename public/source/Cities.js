1764c1e668154e2e7886cc05d6f51eaebe388787
Andrew Boyle <ajboyle31@gmail.com>
2016-08-07 17:15:14 +0000

    finished with pictures, working

--- public/source/Cities.js
+++ public/source/Cities.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
+var cityPics = {
+    "Charlottesville": 'http://az616578.vo.msecnd.net/files/2016/03/19/635939924999229260-1043665980_1400-charlottesville-va-downtown.imgcache.rev1409086909867.web.jpg',
+    "New York": "http://www.nationalgeographic.com/new-york-city-skyline-tallest-midtown-manhattan/assets/img/articleImg/01nyskyline1536.jpg",
+    "Chicago": "http://chicagoraffaello.com/wp-content/uploads/2013/08/chicago-skyline.jpg"
+};
+
 class Cities extends Component {
     render(){
         return (
+            <div className="loc">
             <h3>{this.props.name}, {this.props.state}</h3>
+            <img id="pic" src={cityPics[this.props.name]} />
+            </div>
         );
     }
 }
