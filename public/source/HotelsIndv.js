902d289f80ac82b56e44ca227f7893ecaa028b4b
Andrew Boyle <ajboyle31@gmail.com>
2016-08-08 18:24:44 +0000

    update id

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -5,7 +5,7 @@ class HotelsIndv extends Component {
     
     render(){
         return (
-            <div className="hotels" id={this.props.key}>
+            <div className="hotels" id={this.props.id}>
             <a href={this.props.link} onClick={this.props.hotelCallback.bind(this)}>
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
