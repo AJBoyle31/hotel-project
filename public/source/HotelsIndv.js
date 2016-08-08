f6eeb0fd96d2b93c289a51af2b2d63d3b1bd047b
Andrew Boyle <ajboyle31@gmail.com>
2016-08-08 01:13:12 +0000

    added callback

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -2,13 +2,11 @@ import React, {Component} from 'react';
 
 
 class HotelsIndv extends Component {
-    showHotel(){
-        
-    }
+    
     render(){
         return (
-            <div className="hotels">
-            <a href={this.props.link} onClick={this.showHotel.bind(this)}>
+            <div className="hotels" id={this.props.key}>
+            <a href={this.props.link} onClick={this.props.hotelCallback.bind(this)}>
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
