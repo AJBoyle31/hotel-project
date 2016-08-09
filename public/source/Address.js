c91308a322bd0a3d16fa63870962a7cfe9604613
Andrew Boyle <ajboyle31@gmail.com>
2016-08-09 17:04:43 +0000

    update props

--- public/source/Address.js
+++ public/source/Address.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Address extends Component {
     render(){
         return (
-            <h5 id="address">{this.props.address.line1}, {this.props.address.city} {this.props.hotel.address.region_code} {this.props.hotel.address.postal_code}</h5>
+            <h5 id="address">{this.props.address.line1}, {this.props.address.city} {this.props.address.region_code} {this.props.address.postal_code}</h5>
         );
     }
 }
