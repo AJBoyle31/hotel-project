d874e2064e9edd088f12710264eb49fc50381244
Andrew Boyle <ajboyle31@gmail.com>
2016-08-09 01:25:35 +0000

    working on single hotel api call

--- public/source/HotelsList.js
+++ public/source/HotelsList.js
@@ -9,6 +9,7 @@ const API_HEADERS = {
     'Content-Type': 'application/json'
 };
 //'https://hotel-project-ajboyle.c9users.io/api/locations/' + city + '/hotels/' + hotel.id + '?checkin=' + checkin + '&checkout=' + checkout
+//http://localhost:9696/api/locations/ charlottesville /hotels/ 0ZEzgGG4W04s8EP05g9krVMw ?checkin= 2015-05-02 &checkout= 2015-05-04
 
 
 class HotelsList extends Component {
@@ -19,17 +20,18 @@ class HotelsList extends Component {
             showHotel: false
         };
     }
-    getHotel(){
-        fetch(API_URL + this.props.info.city + '/hotels/' + this.hotel.id + '?checkin=' + this.props.info.checkin + '&checkout=' + this.props.info.checkout, {headers: API_HEADERS})
+    getHotel(hotelId){
+        fetch(API_URL + this.props.info.city + "/hotels/" + hotelId + "?checkin=" + this.props.info.checkin + "&checkout=" + this.props.info.checkout, {headers: API_HEADERS})
         .then((response) => response.json())
         .then((responseData) => {
             this.setState({ 
-                hotel: responseData, 
+                hotel: <Hotel hotel={responseData} />, 
                 showHotel: true
             });
         })
         .catch((error) => {
-            console.log('Error fetching and parsing data', error);
+            
+            console.log('Error fetching and parsing data hotel ' + hotelId , error);
         });
     }
     render(){
@@ -53,7 +55,7 @@ class HotelsList extends Component {
         return (
             <div className="results">
             <div id='hotels' className={this.state.showHotel ? "hotelsHide" : ""}>{hotelNodes}</div>
-            
+            {this.state.hotel}
             </div>
         );
     }   
