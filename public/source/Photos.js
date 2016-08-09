5236bd9b8c05041918a2ea3561e0691e23369ad4
Andrew Boyle <ajboyle31@gmail.com>
2016-08-09 15:06:02 +0000

    corrected style error

--- public/source/Photos.js
+++ public/source/Photos.js
@@ -11,21 +11,33 @@ class Photos extends Component {
         var x = this.props.pics;
         if (n > x.length) { slideIndex = 1 }
         if (n < 1) {slideIndex = x.length}
-        for (i=0; i < x.length; i++){
-            x[i].style.display = "none";
-        }
-        x[slideIndex - 1].style.display = "block";
+        //for (i=0; i < x.length; i++){
+        //    x[i].style.display = "none";
+        //}
+        //x[slideIndex - 1].style.display = "block";
     }
     render(){
         var photos = this.props.pics.map((photo) => {
-            return <img className="slideShow" src={photo.url} />;
+            return <img className="slideShow" src={photo.url} key={photo.url} />;
         });
+        
+        var styleLeft = {
+            position: "absolute",
+            top: '45%',
+            left: 0
+        };
+        var styleRight = {
+            position: "absolute",
+            top: '45%',
+            right: 0
+        };
+        
         
         return (
             <div className="picture">
                 {photos}
-                <a class="btn-floating" style="position:absolute;top:45%;left:0" onClick={this.plusDivs(-1).bind(this)}>&#10094;</a>
-                <a class="btn-floating" style="position:absolute;top:45%;right:0" onClick={this.plusDivs(+1).bind(this)}>&#10095;</a>
+                <a className="btn-floating" style={styleLeft} onClick={this.plusDivs(-1)}>&#10094;</a>
+                <a className="btn-floating" style={styleRight} onClick={this.plusDivs(+1)}>&#10095;</a>
             </div>        
         );
     }
