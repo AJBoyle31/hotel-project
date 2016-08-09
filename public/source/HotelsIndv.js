6ca787fb35f3dfa4feaba1a15bdae783afaa07ca
Andrew Boyle <ajboyle31@gmail.com>
2016-08-09 01:24:45 +0000

    updated onClick callback

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 
 
 class HotelsIndv extends Component {
-    
     render(){
+        var hotelId = this.props.id;
         return (
             <div className="hotels" id={this.props.id}>
-            <a href={this.props.link} onClick={this.props.hotelCallback.bind(this)}>
+            <a href={this.props.link} onClick={this.props.hotelCallback.bind(this, this.props.id)}>
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
