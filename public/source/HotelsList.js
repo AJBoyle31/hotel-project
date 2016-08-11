20150f80c206ad6dbed4940000b3688afe30a21c
Andrew Boyle <ajboyle31@gmail.com>
2016-08-11 00:45:16 +0000

    removed callback on HotelsIndv, removed api

--- public/source/HotelsList.js
+++ public/source/HotelsList.js
@@ -4,42 +4,18 @@ import FilterOptions from './FilterOptions';
 import Hotel from './Hotel';
 import 'whatwg-fetch';
 
-const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-const API_HEADERS = {
-    'Content-Type': 'application/json'
-};
 //'https://hotel-project-ajboyle.c9users.io/api/locations/' + city + '/hotels/' + hotel.id + '?checkin=' + checkin + '&checkout=' + checkout
 //http://localhost:9696/api/locations/ charlottesville /hotels/ 0ZEzgGG4W04s8EP05g9krVMw ?checkin= 2015-05-02 &checkout= 2015-05-04
 
 
 class HotelsList extends Component {
-    constructor(){
-        super();
-        this.state = {
-            hotel: [],
-            showHotel: false
-        };
-    }
-    getHotel(hotelId){
-        fetch(API_URL + this.props.info.city + "/hotels/" + hotelId + "?checkin=" + this.props.info.checkin + "&checkout=" + this.props.info.checkout, {headers: API_HEADERS})
-        .then((response) => response.json())
-        .then((responseData) => {
-            this.setState({ 
-                hotel: <Hotel hotel={responseData} />, 
-                showHotel: true
-            });
-        })
-        .catch((error) => {
-            
-            console.log('Error fetching and parsing data hotel ' + hotelId , error);
-        });
-    }
+    
     render(){
-        var city = this.props.info.city;
-        var checkin = this.props.info.checkin;
-        var checkout = this.props.info.checkout;
+        var city = this.props.hotelData.city;
+        var checkin = this.props.hotelData.checkin;
+        var checkout = this.props.hotelData.checkout;
         
-        let hotelNodes = this.props.data.map((hotel) => {
+        let hotelNodes = this.props.hotelData.hotelsSearch.map((hotel) => {
             return (
                 <HotelsIndv 
                     key={hotel.id}
@@ -47,15 +23,15 @@ class HotelsList extends Component {
                     link={'#api/locations/' + city + '/hotels/' + hotel.id + '?checkin=' + checkin + '&checkout=' + checkout} 
                     photo={hotel.photos[0]['thumbnail']} 
                     name={hotel.name} rate={hotel.available ? '$' + (Number(hotel['nightly_rate']).toFixed(0)) : "Unavailable"}
-                    hotelCallback={this.getHotel.bind(this)}
+                    
                 />
             );
         });
                 
         return (
             <div className="results">
-            <div id='hotels' className={this.state.showHotel ? "hotelsHide" : ""}>{hotelNodes}</div>
-            {this.state.hotel}
+            <div id='hotels'>{hotelNodes}</div>
+         
             </div>
         );
     }   
