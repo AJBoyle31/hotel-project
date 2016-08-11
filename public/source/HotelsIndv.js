0e7f293d17969226dfd47ac28e12d948ecef0f54
Andrew Boyle <ajboyle31@gmail.com>
2016-08-11 00:44:04 +0000

    removed onclick

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -6,7 +6,7 @@ class HotelsIndv extends Component {
         var hotelId = this.props.id;
         return (
             <div className="hotels" id={this.props.id}>
-            <a href={this.props.link} onClick={this.props.hotelCallback.bind(this, this.props.id)}>
+            <a href={this.props.link} >
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
