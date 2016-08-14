eae2f387dc734f6cff5d30c7c86ada0af7d6dfbb
Andrew Boyle <ajboyle31@gmail.com>
2016-08-14 23:35:44 +0000

    corrected href

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react';
 
 
 class HotelsIndv extends Component {
+    handleEvent(hotelId){
+        this.props.taskCallbacks.getHotel(hotelId);
+    }
     render(){
-        var hotelId = this.props.id;
+        
         return (
             <div className="hotels" id={this.props.id}>
-            <a href={this.props.link} >
+            <a href={this.props.link} onClick={this.handleEvent.bind(this.props.id)} >
             <img src={this.props.photo} />
             <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
             <h3 className='hotelrate'>{this.props.rate}</h3>
