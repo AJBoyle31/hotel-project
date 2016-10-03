a2c944a80abd0c358bab9361fb262bdbc07f06b4
Andrew Boyle <ajboyle31@gmail.com>
2016-10-03 01:03:17 +0000

    build

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -70,8 +70,8 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	//const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-	var API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
+	var API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
+	//const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
 	var API_HEADERS = {
 	    'Content-Type': 'application/json'
 	};
@@ -195,82 +195,6 @@
 	}(_react.Component);
 
 	_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById("app"));
-
-	//function to validate the check in and out dates entered by user to make sure they are the correct format and the 
-	//check out date is after the check in date
-	function validateDate(x, y) {
-	    //regex /\d{4}-\d{2}-\d{2}/ 
-	    //x is checkin
-	    //y is checkout
-	    var regex = /\d{4}-\d{2}-\d{2}/;
-
-	    var xYear = x.slice(0, 4);
-	    var xMonth = x.slice(5, 7);
-	    var xDay = x.slice(8, 10);
-
-	    var yYear = y.slice(0, 4);
-	    var yMonth = y.slice(5, 7);
-	    var yDay = y.slice(8, 10);
-
-	    if (!regex.test(x)) {
-	        alert('Invalid Date');
-	        return false;
-	    }
-	    if (!regex.test(y)) {
-	        alert('Invalid Date');
-	        return false;
-	    }
-	    if (yYear > xYear) {
-	        return true;
-	    } else {
-	        if (yMonth > xMonth) {
-	            return true;
-	        } else {
-	            if (yDay > xDay) {
-	                return true;
-	            } else {
-	                alert("Invalid Date Range");
-	                return false;
-	            }
-	        }
-	    }
-	}
-
-	//for slideshow on hotel view
-	var slideIndex = 1;
-
-	function plusDivs(n) {
-	    showDivs(slideIndex += n);
-	}
-
-	//handles slideshow of pictures
-	function showDivs(n) {
-	    var i;
-	    var x = document.getElementsByClassName("slideShow");
-	    if (n > x.length) {
-	        slideIndex = 1;
-	    }
-	    if (n < 1) {
-	        slideIndex = x.length;
-	    }
-	    for (i = 0; i < x.length; i++) {
-	        x[i].style.display = "none";
-	    }
-	    x[slideIndex - 1].style.display = "block";
-	}
-
-	//handles the star rating of the hotel
-	function stars() {
-	    var star = $('.stars').prop('id');
-	    star = Number(star);
-	    var arr = ['a', 'b', 'c', 'd', 'e'];
-
-	    for (var m = 0; m < 6; m++) {
-	        if (m <= star) {
-	            $('#' + arr[m]).html("&#9733");
-	        }
-	    }
-	}
 
 /***/ },
 /* 1 */
@@ -21513,11 +21437,47 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	//const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-	var API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
+	var API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
+	//const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/'
 	var API_HEADERS = {
 	    'Content-Type': 'application/json'
 	};
+
+	//function to validate the check in and out dates entered by user to make sure they are the correct format and the 
+	//check out date is after the check in date
+	function validateDate(x, y) {
+	    //x is checkin
+	    //y is checkout
+	    var xYear = Number(x.slice(0, 4));
+	    var xMonth = Number(x.slice(5, 7));
+	    var xDay = Number(x.slice(8, 10));
+
+	    var yYear = Number(y.slice(0, 4));
+	    var yMonth = Number(y.slice(5, 7));
+	    var yDay = Number(y.slice(8, 10));
+
+	    if (xYear > yYear) {
+	        console.log(xYear);
+	        alert("Invalid Date Range");
+	        return false;
+	    } else {
+	        if (xMonth > yMonth) {
+	            console.log(xMonth);
+	            alert("Invalid Date Range");
+	            return false;
+	        } else {
+	            if (xDay > yDay) {
+	                alert("Invalid Date Range");
+	                console.log("checkin is " + xDay);
+	                console.log("checkout is " + yDay);
+	                return false;
+	            } else {
+
+	                return true;
+	            }
+	        }
+	    }
+	}
 
 	//Get information from user regarding city, checkin and checkout dates. 
 
@@ -21536,9 +21496,13 @@
 	            var city = event.target.city.value;
 	            var checkin = event.target.checkin.value;
 	            var checkout = event.target.checkout.value;
-	            this.props.taskCallbacks.clearHotel;
-	            this.props.taskCallbacks.getHotelsSearch(city, checkin, checkout);
-	            event.preventDefault();
+	            if (validateDate(checkin, checkout)) {
+	                this.props.taskCallbacks.clearHotel;
+	                this.props.taskCallbacks.getHotelsSearch(city, checkin, checkout);
+	                event.preventDefault();
+	            } else {
+	                event.preventDefault();
+	            }
 	        }
 	    }, {
 	        key: 'render',
