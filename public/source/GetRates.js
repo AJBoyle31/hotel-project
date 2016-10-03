d331a6f36fb5b014fb220b79e7db5f0d783a42c7
Andrew Boyle <ajboyle31@gmail.com>
2016-10-03 01:03:50 +0000

    added date validation

--- public/source/GetRates.js
+++ public/source/GetRates.js
@@ -4,12 +4,49 @@ import Hotel from './Hotel';
 import 'whatwg-fetch';
 import datepicker from 'bootstrap-datepicker';
 
-//const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/'
+const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
+//const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/'
 const API_HEADERS = {
     'Content-Type': 'application/json'
 };
 
+//function to validate the check in and out dates entered by user to make sure they are the correct format and the 
+//check out date is after the check in date
+function validateDate(x, y) {
+    //x is checkin
+    //y is checkout
+    var xYear = Number(x.slice(0,4));
+    var xMonth = Number(x.slice(5,7));
+    var xDay = Number(x.slice(8,10));
+
+    var yYear = Number(y.slice(0,4));
+    var yMonth = Number(y.slice(5,7));
+    var yDay = Number(y.slice(8,10));
+    
+    if (xYear > yYear) {
+        console.log(xYear);
+        alert("Invalid Date Range");
+        return false;
+    }
+    else {
+        if (xMonth > yMonth){
+            console.log(xMonth);
+            alert("Invalid Date Range");
+            return false;
+        }
+        else {
+            if (xDay > yDay){
+                alert("Invalid Date Range");
+                console.log("checkin is " + xDay);
+                console.log("checkout is " + yDay);
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+}
 
 //Get information from user regarding city, checkin and checkout dates. 
 class GetRates extends Component {
@@ -18,9 +55,13 @@ class GetRates extends Component {
         let city = event.target.city.value;
         let checkin = event.target.checkin.value;
         let checkout = event.target.checkout.value;
-        this.props.taskCallbacks.clearHotel;
-        this.props.taskCallbacks.getHotelsSearch(city, checkin, checkout);
-        event.preventDefault();
+        if (validateDate(checkin, checkout)){
+            this.props.taskCallbacks.clearHotel;
+            this.props.taskCallbacks.getHotelsSearch(city, checkin, checkout);
+            event.preventDefault();
+        } else {
+            event.preventDefault();
+        }
     }
     render(){
         
