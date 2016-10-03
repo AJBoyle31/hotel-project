2cb671932233b3179d76422d421319d7105d666b
Andrew Boyle <ajboyle31@gmail.com>
2016-10-03 01:03:39 +0000

    removed unnecessary code

--- public/source/App.js
+++ public/source/App.js
@@ -4,8 +4,8 @@ import Nav from './Nav';
 import 'whatwg-fetch';
 
 
-//const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
+const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
+//const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
 const API_HEADERS = {
     'Content-Type': 'application/json'
 };
@@ -103,84 +103,3 @@ class App extends Component {
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
-
-
-
-
-
-
-
-//function to validate the check in and out dates entered by user to make sure they are the correct format and the 
-//check out date is after the check in date
-function validateDate(x, y) {
-    //regex /\d{4}-\d{2}-\d{2}/ 
-    //x is checkin
-    //y is checkout
-    var regex = /\d{4}-\d{2}-\d{2}/;
-    
-    var xYear = x.slice(0,4);
-    var xMonth = x.slice(5,7);
-    var xDay = x.slice(8,10);
-    
-    var yYear = y.slice(0,4);
-    var yMonth = y.slice(5,7);
-    var yDay = y.slice(8,10);
-    
-    if (!regex.test(x)) {
-        alert('Invalid Date');
-        return false;
-    } 
-    if (!regex.test(y)) {
-        alert('Invalid Date');
-        return false;
-    }
-    if (yYear > xYear) {
-        return true;
-    }
-    else {
-        if (yMonth > xMonth){
-            return true;
-        }
-        else {
-            if (yDay > xDay){
-                return true;
-            }
-            else {
-                alert("Invalid Date Range");
-                return false;
-            }
-        }
-    }
-     
-}
-
-//for slideshow on hotel view
-var slideIndex = 1;
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-//handles slideshow of pictures
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slideShow");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
-//handles the star rating of the hotel
-function stars() {
-    var star = $('.stars').prop('id');
-    star = Number(star);
-    var arr = ['a','b','c','d','e'];
-    
-    for (var m = 0; m < 6; m++) { 
-        if (m <= star) { $('#' + arr[m]).html("&#9733"); } 
-    }
-    
-}
