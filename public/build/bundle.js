1ce1f26b39ae8c0eca316efacfca7957a9fa3833
Andrew Boyle <ajboyle31@gmail.com>
2016-10-06 20:10:10 +0000

    build

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -70,8 +70,8 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
-	//const API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
+	//const API_URL = 'https://hotel-project-ajboyle.c9users.io/api/locations/';
+	var API_URL = 'https://ajbhotelpage.herokuapp.com/api/locations/';
 	var API_HEADERS = {
 	    'Content-Type': 'application/json'
 	};
