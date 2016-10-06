e6d94b314bcc92fe1c18543491c0676074f42287
Andrew Boyle <ajboyle31@gmail.com>
2016-10-06 20:03:52 +0000

    added className to div

--- public/source/HotelsIndv.js
+++ public/source/HotelsIndv.js
@@ -24,7 +24,7 @@ class HotelsIndv extends Component {
         
         return (
             <div className="hotels">
-                <div id={this.props.id}>
+                <div id={this.props.id} className="eachHotel">
                     <a href={this.props.link} onClick={this.open} >
                     <img src={this.props.photo} />
                     <h3 className='hotelname'>{this.props.name.length > 20 ? this.props.name.slice(0,20) + '...' : this.props.name}</h3>
